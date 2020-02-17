45520536fb80fc8e9160596f0a451b3d6921ddad
Iban Avellaneda <iavellanedadev@gmail.com>
2020-02-17 11:53:17 -0500

    added viewcontroller

---
+++ CapstoneApp/ViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  CapstoneApp
+//
+//  Created by Consultant on 2/17/20.
+//  Copyright © 2020 Consultant. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
