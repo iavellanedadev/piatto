c2c53224176800960737a6071a54d3bdce260217
Iban Avellaneda <iavellanedadev@gmail.com>
2020-02-17 11:55:10 -0500

    edited loginVC w different message

--- CapstoneApp/ViewController/LoginViewController.swift
+++ CapstoneApp/ViewController/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("different loggin in message!")
         // Do any additional setup after loading the view.
     }
     
