33a5f330ac0fcd1b663ed7492319b55c75fa98a8
Iban Avellaneda <iavellanedadev@gmail.com>
2020-02-17 11:54:30 -0500

    edited loginVC

--- CapstoneApp/ViewController/LoginViewController.swift
+++ CapstoneApp/ViewController/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("logging in!")
         // Do any additional setup after loading the view.
     }
     
